553c01186c26917dea523a3a43d591bd34b534f6
Love <mzeelovegeneral@gmail.com>
2023-10-19 16:21:46 +0100

    implement add stack fucntionality

--- monty.h
+++ monty.h
@@ -64,6 +64,7 @@ void push_to_stack(stack_t **stack, unsigned int line_number);
 void print_all(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **head, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_cnt);
+void swap(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
 
 #endif
