e796069b2400578e105fe5276d5413731fa2d96f
Love <mzeelovegeneral@gmail.com>
2023-10-19 16:33:55 +0100

    add nop function

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop -  do not do anything nothing
+ * @stack: passed from main, but gonna be voided
+ * @line_number: also gonna be voided
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
